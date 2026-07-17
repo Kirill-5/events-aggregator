b9eeb9880261810433beba5908a84d59e9278004
Kirill-5 <solovyanovkd@gmai.com>
2026-07-17 09:52:30 +0500

    Use POSTGRES_CONNECTION_STRING

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,3 @@
 import os
 
-DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
-DB_USER = os.getenv("POSTGRES_USERNAME")
-DB_PASS = os.getenv("POSTGRES_PASSWORD")
-DB_NAME = os.getenv("POSTGRES_DATABASE_NAME")
-
-print(f"DB_HOST: {DB_HOST}")
-print(f"DB_PORT: {DB_PORT}")
-print(f"DB_NAME: {DB_NAME}")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("POSTGRES_CONNECTION_STRING")
