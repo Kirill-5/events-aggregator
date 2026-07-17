83e1871c10834d90da64ecee443a2e25012ce8a8
Kirill-5 <solovyanovkd@gmai.com>
2026-07-17 09:42:53 +0500

    Add debug prints for DB env vars

--- app/core/config.py
+++ app/core/config.py
@@ -6,4 +6,8 @@ DB_USER = os.getenv("POSTGRES_USERNAME")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DATABASE_NAME")
 
+print(f"DB_HOST: {DB_HOST}")
+print(f"DB_PORT: {DB_PORT}")
+print(f"DB_NAME: {DB_NAME}")
+
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
