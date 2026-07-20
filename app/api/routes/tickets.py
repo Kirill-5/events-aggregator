472ad271b370033b96757b4c6a53b3ab3ab0091a
Kirill-5 <solovyanovkd@gmai.com>
2026-07-20 15:59:18 +0500

    Use CreateTicketUsecase

--- app/api/routes/tickets.py
+++ app/api/routes/tickets.py
@@ -1,65 +1,56 @@
+from uuid import UUID
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+
+from app.core.config import EVENTS_PROVIDER_API_KEY, EVENTS_PROVIDER_URL
 from app.db.database import get_db
 from app.repositories.event_repository import EventRepository
 from app.repositories.ticket_repository import TicketRepository
-from app.services.events_provider_client import EventsProviderClient
 from app.schemas.ticket import TicketCreate, TicketResponse
-from uuid import UUID
-from app.core.config import EVENTS_PROVIDER_URL, EVENTS_PROVIDER_API_KEY
+from app.services.events_provider_client import EventsProviderClient
+from app.usecases.create_ticket import CreateTicketUsecase
 
 router = APIRouter(tags=["tickets"])
 
 @router.post("/api/tickets", status_code=status.HTTP_201_CREATED)
 async def create_ticket(
-        ticket: TicketCreate,
-        db: Session = Depends(get_db)
+    ticket: TicketCreate,
+    db: Session = Depends(get_db)
 ):
-
     try:
         UUID(ticket.event_id)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid event_id format")
 
-    event = EventRepository(db).get(ticket.event_id)
-    if not event:
-        raise HTTPException(status_code=404, detail="Event not found")
-
-    if event.status != "published":
-        raise HTTPException(status_code=400, detail="Event is not published")
-
     client = EventsProviderClient(
         base_url=EVENTS_PROVIDER_URL,
         api_key=EVENTS_PROVIDER_API_KEY
     )
-    client_reg = await client.register(
-        event_id=ticket.event_id,
-        first_name=ticket.first_name,
-        last_name=ticket.last_name,
-        email=ticket.email,
-        seat=ticket.seat
-    )
-
-    ticket_id = client_reg.get("ticket_id")
-    if not ticket_id:
-        raise HTTPException(status_code=500, detail="Failed to get ticket_id from provider")
-
+    event_repo = EventRepository(db)
     ticket_repo = TicketRepository(db)
-    ticket_repo.create(
-        event_id=ticket.event_id,
-        ticket_id=ticket_id,
-        first_name=ticket.first_name,
-        last_name=ticket.last_name,
-        email=ticket.email,
-        seat=ticket.seat
-    )
 
-    return {"ticket_id": ticket_id}
+    #UseCase
+    usecase = CreateTicketUsecase(client, event_repo, ticket_repo)
+
+    try:
+        ticket_id = await usecase.do(
+            event_id=ticket.event_id,
+            first_name=ticket.first_name,
+            last_name=ticket.last_name,
+            email=ticket.email,
+            seat=ticket.seat
+        )
+        return {"ticket_id": ticket_id}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/api/tickets/{ticket_id}")
 async def delete_ticket(
-        ticket_id: str,
-        db: Session = Depends(get_db)
+    ticket_id: str,
+    db: Session = Depends(get_db)
 ):
     ticket_repo = TicketRepository(db)
     ticket = ticket_repo.get(ticket_id)
