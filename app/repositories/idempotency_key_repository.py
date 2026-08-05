9c719c8cf050ca1b6c8438c076fbedc34bb3be26
Kirill-5 <solovyanovkd@gmai.com>
2026-08-05 17:01:16 +0500

    Add debug print to force rebuild

--- app/repositories/idempotency_key_repository.py
+++ app/repositories/idempotency_key_repository.py
@@ -8,21 +8,18 @@ class IdempotencyKeyRepository:
     def __init__(self, session: Session):
         self.session = session
 
+    def get_by_key(self, key: str) -> Optional[IdempotencyKey]:
+        print(f"Searching for key: {key}")
+        return self.session.query(IdempotencyKey).filter(IdempotencyKey.key == key).first()
 
-
-def get_by_key(self, key: str) -> Optional[IdempotencyKey]:
-    print(f"Searching for key: {key}")
-    return self.session.query(IdempotencyKey).filter(IdempotencyKey.key == key).first()
-
-def save(self, key: str, ticket_id: UUID, event_id: UUID, seat: str) -> IdempotencyKey:
-    new_key = IdempotencyKey(
-        key=key,
-        ticket_id=ticket_id,
-        event_id=event_id,
-        seat=seat
-
-    )
-    self.session.add(new_key)
-    self.session.commit()
-    self.session.refresh(new_key)
-    return new_key
+    def save(self, key: str, ticket_id: UUID, event_id: UUID, seat: str) -> IdempotencyKey:
+        new_key = IdempotencyKey(
+            key=key,
+            ticket_id=ticket_id,
+            event_id=event_id,
+            seat=seat
+        )
+        self.session.add(new_key)
+        self.session.commit()
+        self.session.refresh(new_key)
+        return new_key
