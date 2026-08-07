fc37fccfe4d9696dc2e4ca5c539fb5e99fd8018a
Kirill-5 <solovyanovkd@gmai.com>
2026-08-07 12:25:41 +0500

    chore: remove redundant inline comments from events_paginator.py

--- app/services/events_paginator.py
+++ app/services/events_paginator.py
@@ -11,26 +11,26 @@ class EventsPaginator:
         return self
 
     async def __anext__(self):
-        # если текущая страница пуста или закончилась, то загружаем следующую
+
         if not self.current_page or self.page_index >= len(self.current_page):
             data = await self.client.events(cursor=self.cursor, changed_at=self.changed_at)
             self.current_page = data.get("results", [])
             self.cursor = data.get("next")
             self.page_index = 0
 
-            # Если нет ни текущей страницы ни следующей, - завершаем итерацию
+
             if not self.current_page and not self.cursor:
                 raise StopAsyncIteration
 
-        # если текущая страница пуста, но есть следующая, то повторяем загрузку
+
         if not self.current_page:
             return await self.__anext__()
 
-        # Возвращаем один элемент и двигаем индекс
+
         event = self.current_page[self.page_index]
         self.page_index += 1
 
-        # если это был последний элемент на странице, тогда обнуляем страницу
+
         if self.page_index >= len(self.current_page):
             self.current_page = []
 
