6d1ed0c03ce843d24238e622ea589d1ecb255300
Kirill-5 <solovyanovkd@gmai.com>
2026-08-07 12:36:35 +0500

    fix: export SyncMetadataRepository in repositories/__init__.py

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,7 +1,15 @@
-__all__ = ["EventRepository", "PlaceRepository", "TicketRepository", "OutboxRepository", "IdempotencyKeyRepository"]
-
 from app.repositories.event_repository import EventRepository
 from app.repositories.place_repository import PlaceRepository
 from app.repositories.ticket_repository import TicketRepository
-from app.repositories.outbox_repository import OutboxRepository
 from app.repositories.idempotency_key_repository import IdempotencyKeyRepository
+from app.repositories.outbox_repository import OutboxRepository
+from app.repositories.sync_metadata_repository import SyncMetadataRepository
+
+__all__ = [
+    "EventRepository",
+    "PlaceRepository",
+    "TicketRepository",
+    "IdempotencyKeyRepository",
+    "OutboxRepository",
+    "SyncMetadataRepository",
+]
