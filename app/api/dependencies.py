15cfbbc08caf02b8fcf4d3f1cf072ffb905ce4b4
Kirill-5 <solovyanovkd@gmai.com>
2026-08-07 12:24:39 +0500

    fix: move EventRepository import to module level

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -5,6 +5,7 @@ from fastapi import Depends
 from app.services.events_provider_client import EventsProviderClient
 from app.core.config import EVENTS_PROVIDER_URL, EVENTS_PROVIDER_API_KEY
 from app.repositories.ticket_repository import TicketRepository
+from app.repositories.event_repository import EventRepository
 from app.usecases.cancel_ticket import CancelTicketUsecase
 from app.usecases.get_seats import GetSeatsUsecase
 from app.db.database import SessionLocal
@@ -29,7 +30,6 @@ def get_cancel_ticket_usecase(
 def get_seats_usecase(
     client: EventsProviderClient = Depends(get_events_provider_client),
 ) -> GetSeatsUsecase:
-    from app.repositories.event_repository import EventRepository
     db = SessionLocal()
     event_repo = EventRepository(db)
     return GetSeatsUsecase(client, event_repo)
