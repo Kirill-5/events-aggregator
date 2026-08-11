b1d379526278c17db1b6859436e5bc24c2795a59
Kirill-5 <solovyanovkd@gmai.com>
2026-08-11 12:35:57 +0500

    fix: add missing String import and remove extra spaces in place.py

--- app/models/place.py
+++ app/models/place.py
@@ -1,12 +1,15 @@
-from app.db.database import Base
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String
 import uuid
+
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.db.database import Base
+
 
 class Place(Base):
     __tablename__ = "place"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String)
     city = Column(String)
-    address = Column(String , nullable=True)
-    seats_pattern  = Column(String, nullable=True)
+    address = Column(String, nullable=True)
+    seats_pattern = Column(String, nullable=True)
